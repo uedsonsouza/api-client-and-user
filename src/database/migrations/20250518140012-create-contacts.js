b7d250475bee904dd4d6f80d350e33a68f89a227
uedsonsouza <gaiek2016rmc@gmail.com>
2025-06-30 09:39:40 -0300

    create migrations

---
+++ src/database/migrations/20250518140012-create-contacts.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("contacts", {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            customer_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: "customers",
+                    key: "id",
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+                allowNull: false,
+            },
+        });
+    },
+
+    async down(queryInterface) {
+        await queryInterface.dropTable("users");
+    },
+};

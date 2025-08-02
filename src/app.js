2984c294eac9621fc81445991c5ae6e07941f192
uedsonsouza <gaiek2016rmc@gmail.com>
2025-08-02 12:21:57 -0300

    config app

--- src/app.js
+++ src/app.js
@@ -1,22 +1,41 @@
-import express from 'express';
-import routes from './routes';
-import './database';
+import express from "express";
+import routes from "./routes";
+import "express-async-errors";
+import Youch from "youch";
+import * as Sentry from "@sentry/node";
+import "./database";
+import "dotenv/config";
+import sentryConfig from "./config/sentry";
 
 class App {
     constructor() {
         this.server = express();
+        Sentry.init(sentryConfig);
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
-    middlewares() {
+    middlewares() { 
         this.server.use(express.json());
+        this.server.use(express.urlencoded({ extended: false }));
     }
 
     routes() {
         this.server.use(routes);
+        Sentry.setupExpressErrorHandler(this.server);
     }
 
+    exceptionHandler() {
+        this.server.use(async (err, req, res) => {
+            if (process.env.NODE_ENV === "development") {
+                const errors = await new Youch(err, req).toJSON();
+                return res.status(500).json(errors);
+            }
+
+            return res.status(500).json({ error: "Internal server error" });
+        });
+    }
 }
 
 export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,36 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import auth from './app/middlewares/auth';
 import CustomersController from './app/controllers/CustomersController';
+import ContactsController from './app/controllers/ContactsController';
+import UserController from './app/controllers/UserController';
+import SessionsController from './app/controllers/SessionsController';
+import FilesController from './app/controllers/FilesController';
+
 const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/sessions', SessionsController.create);
+
+routes.use(auth);
 
 routes.get('/customers', CustomersController.index);
+routes.get('/customers/:customer_id/contacts', ContactsController.index);
+routes.get('/customers/:customer_id/contacts/:id', ContactsController.show);
 routes.get('/customers/:id', CustomersController.show);
+routes.post('/customers/:customer_id/contacts', ContactsController.create);
 routes.post('/customers', CustomersController.create);
+routes.put('/customers/:customer_id/customerId/:id', ContactsController.update);
 routes.put('/customers/:id', CustomersController.update);
+routes.delete('/customers/:customer_id/customerId/:id', ContactsController.destroy);
 routes.delete('/customers/:id', CustomersController.destroy);
 
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.post('/users', UserController.create);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.destroy);
+
+routes.post('/files', upload.single('file'), FilesController.create)
 export default routes;

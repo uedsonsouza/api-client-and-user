11ca2edd26b9aade3096ae9334e62a61842ee263
uedsonsouza <uedsonmoc@gmail.com>
2025-08-16 19:13:43 -0300

    config setup united tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,15 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  { files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
 ]);
